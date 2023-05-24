cd9ca91b3be3fc00d06b0dd77a95db101025b66b
Travis <travis.williams94@gmail.com>
2023-05-24 15:44:55 -0600

    complete lab with delete and priority feratures

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    addTodo(e.target.new_task_description.value)
+    form.reset()
+  })
 });
+
+function addTodo(todo){
+  let listItem = document.createElement('li')
+  let btn = document.createElement('button')
+  let select = document.querySelector('select')
+  btn.addEventListener('click', deleteTask)
+  btn.textContent = 'x'
+  listItem.textContent = `${todo} ` 
+  listItem.appendChild(btn)
+  document.querySelector('#tasks').appendChild(listItem)
+
+  if (select.value === "High"){
+    listItem.style.color = 'red';
+  }
+else if (select.value === 'Medium'){
+    listItem.style.color = 'yellow';
+}
+else if (select.value === 'Low'){
+  listItem.style.color = 'green';
+}
+
+};
+
+function deleteTask(e){
+  e.target.parentNode.remove()
+}
